307d3663a7cbb67f0d1505e7e19c8d60812c1be3
YYK <75016748+yykkdev@users.noreply.github.com>
2026-07-29 11:22:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'street-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.status === 200) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); }
+        return res;
+      }).catch(() => cached || (e.request.mode === 'navigate' ? caches.match('./index.html') : undefined));
+      return cached || net;
+    })
+  );
+});
