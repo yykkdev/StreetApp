55f261eb4348e03d71ce97bce907b957f5dd42eb
YYK <estedunk@gmail.com>
2026-08-06 16:13:55 +0200

    Redeclenche le deploiement Pages (le run precedent est reste bloque)
    Aucun changement fonctionnel - simple bump de CACHE pour forcer un
    nouveau run de deploiement GitHub Pages, le precedent (#40, re-run)
    etant reste coince en file d'attente sans jamais demarrer.
    
    Bump CACHE -> street-v46.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'street-v45';
+const CACHE = 'street-v46';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
